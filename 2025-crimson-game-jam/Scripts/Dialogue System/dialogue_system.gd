28f9da35234f3cbdb16495184cc98af5864cdf1b
Evan <etbay10@gmail.com>
2025-11-08 09:40:07 -0800

    Added timer UI

--- 2025-crimson-game-jam/Scripts/Dialogue System/dialogue_system.gd
+++ 2025-crimson-game-jam/Scripts/Dialogue System/dialogue_system.gd
@@ -5,10 +5,13 @@ class_name DialogueSystem extends Resource
 var current_state_index: int = 0
 
 func get_next() -> DialogueType:
-	var dialogue_type: DialogueType = dialogue_states[current_state_index].get_next()
-	if !dialogue_type:
-		update_state()
-	return dialogue_type
+	if current_state_index < dialogue_states.size():
+		var dialogue_type: DialogueType = dialogue_states[current_state_index].get_next()
+		if !dialogue_type:
+			update_state()
+		return dialogue_type
+	else:
+		return null
 
 func update_state() -> void:
 	current_state_index = dialogue_states[current_state_index].transition_state_index
