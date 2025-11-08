970baad579759be05e0e4018b6a4bff18e6d41a1
J-Abitria <j.abitria@wsu.edu>
2025-11-07 23:37:36 -0800

    Removed unnecessary functions.

--- 2025-crimson-game-jam/Scripts/Player.gd
+++ 2025-crimson-game-jam/Scripts/Player.gd
@@ -1,34 +1,11 @@
 class_name Player extends CharacterBody2D
 
-enum MOVE_DIRECTION { LEFT, RIGHT, UP, DOWN }
-
 @export var speed: int = 400
-
-var currentDirection: MOVE_DIRECTION
 var interactionHitbox: Node2D
 
 func _ready():
-	currentDirection = MOVE_DIRECTION.UP
 	interactionHitbox = get_node("InteractionHitbox")
 	print("Interaction Hitbox Pos - X: %d Y: %d" % [interactionHitbox.position.x, interactionHitbox.position.y])
-
-func _input(event):
-	if event is InputEventKey and not event.echo and event.is_pressed():
-		match event.keycode:
-			KEY_W:
-				currentDirection = MOVE_DIRECTION.UP
-				print("The new direction is up.")
-			KEY_A:
-				currentDirection = MOVE_DIRECTION.LEFT
-				print("The new direction is left.")
-			KEY_S:
-				currentDirection = MOVE_DIRECTION.DOWN
-				print("The new direction is down.")
-			KEY_D:
-				currentDirection = MOVE_DIRECTION.RIGHT
-				print("The new direction is right.")
-			_:
-				print("No valid direction for this input.")
 
 func updateInteractionPosition():
 	if velocity.x < 0:
