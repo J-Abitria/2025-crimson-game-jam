ab59a3a921a31dcbb9e87516c7594b5907548246
J-Abitria <j.abitria@wsu.edu>
2025-11-07 22:26:20 -0800

    Added ability for player to face different directions.

--- 2025-crimson-game-jam/Scripts/Player.gd
+++ 2025-crimson-game-jam/Scripts/Player.gd
@@ -1,12 +1,33 @@
 class_name Player extends CharacterBody2D
 
+enum MOVE_DIRECTION { LEFT, RIGHT, UP, DOWN }
+
 @export var speed: int = 400
 var screenSize
+var currentDirection: MOVE_DIRECTION = MOVE_DIRECTION.UP
 
 func _ready():
 	screenSize = get_viewport_rect().size
 
-func _physics_process(delta):
+func _input(event):
+	if event is InputEventKey and not event.echo and event.is_pressed():
+		match event.keycode:
+			KEY_W:
+				currentDirection = MOVE_DIRECTION.UP
+				print("The new direction is up.")
+			KEY_A:
+				currentDirection = MOVE_DIRECTION.LEFT
+				print("The new direction is left.")
+			KEY_S:
+				currentDirection = MOVE_DIRECTION.DOWN
+				print("The new direction is down.")
+			KEY_D:
+				currentDirection = MOVE_DIRECTION.RIGHT
+				print("The new direction is right.")
+			_:
+				print("No valid direction for this input.")
+
+func _physics_process(_delta):
 	velocity = Vector2.ZERO
 	
 	if Input.is_action_pressed("moveRight"):
