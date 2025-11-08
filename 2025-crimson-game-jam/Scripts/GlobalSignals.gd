5ba07cd98321ff624e42ab490fa451e6dc7c967e
J-Abitria <j.abitria@wsu.edu>
2025-11-08 13:23:15 -0800

    Linked dialogue box system with the main interactions between NPCs.

--- 2025-crimson-game-jam/Scripts/GlobalSignals.gd
+++ 2025-crimson-game-jam/Scripts/GlobalSignals.gd
@@ -2,6 +2,8 @@ class_name GlobalSignals extends Node
 
 @warning_ignore("unused_signal")
 signal promptNPC
+@warning_ignore("unused_signal")
+signal interactionDone
 
 func _ready():
 	pass
--- 2025-crimson-game-jam/Scripts/NPCMovement.gd
+++ 2025-crimson-game-jam/Scripts/NPCMovement.gd
@@ -1,5 +1,6 @@
 class_name NPC extends CharacterBody2D
 
+@onready var dialogueBox: DialogueBox = get_node("../../../CanvasLayer/Game UI/MarginContainer/DialogueBox")
 @export var npcSpeed: int = 100
 var pathData: PathFollow2D
 
@@ -12,3 +13,6 @@ func _physics_process(delta):
 
 func _on_prompted():
 	print("Interaction Received")
+	dialogueBox.visible = true
+	dialogueBox.proceed_dialogue()
+	
