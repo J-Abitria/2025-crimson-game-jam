d89cd0647f2de7fd4f9a1059f91a8bfbb2c0a9ec
J-Abitria <j.abitria@wsu.edu>
2025-11-09 19:15:00 -0800

    Set the timer to end the level after 5 minutes.

--- 2025-crimson-game-jam/Scripts/UI/timer_label.gd
+++ 2025-crimson-game-jam/Scripts/UI/timer_label.gd
@@ -9,6 +9,10 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	var time_elapsed = Time.get_ticks_msec() - start_time
 	var time_remaining = time_alloted_minutes * 60000 - time_elapsed
+	
+	if time_remaining <= 0:
+		get_tree().change_scene_to_file("res://Scenes/Ending.tscn")
+	
 	if (time_remaining / 1000) % 60 < 10:
 		text = str(time_remaining / 60000) + ":0"
 	else:
