6b3b15561bec327dc50adf0b948ff249d0cb344f
VortexCoreCoding <logan.spinali@wsu.edu>
2025-11-09 16:36:24 -0800

    detecting npcs talking

--- 2025-crimson-game-jam/Scripts/LineOfSight.gd
+++ 2025-crimson-game-jam/Scripts/LineOfSight.gd
@@ -1,6 +1,7 @@
 class_name LineOfSight extends Area2D
 
 @export var npc: NPC
+var timer = 5.0
 
 func _ready():
 	npc.direction_change.connect(Callable(self, "detect_change"))
@@ -8,16 +9,23 @@ func _ready():
 func _process(_delta):
 	var overlappingBodies = get_overlapping_bodies()
 	
-	for body in overlappingBodies:
-		if body is Player:
-			if body.heldDrink != "":
-				print("The player is currently holding a %s" % [body.heldDrink])
-			if body.heldItem != "":
-				print("The player is currently holding a %s" % [body.heldItem])
-			break
+	if timer >= 5.0:
+		for body in overlappingBodies:
+			if body is Player:
+				if body.heldDrink != "":
+					print("The player is currently holding a %s" % [body.heldDrink])
+				if body.heldItem != "":
+					print("The player is currently holding a %s" % [body.heldItem])
+				if !npc.isInteracting and body.isInteracting:
+					npc.change_love(-20)
+				break
+	else:
+		if (timer <= 0):
+			timer = 5
+		else:
+			timer -= _delta
 
 func detect_change(direction : Vector2i) :
-	print(direction.x, ", ", direction.y)
 	if (direction.x == 1):
 		rotation = PI/2
 	elif (direction.x == -1):
